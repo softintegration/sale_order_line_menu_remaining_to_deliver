68d45068cb8b79678257f7063c020558fd4debd1
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-10 07:06:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sale order line menu with remaining to deliver field
+----------------------------------------------------
+This module add the remaining to deliver field to sale order line menu.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale order line menu with remaining to deliver field',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'description': "",
+    'depends': [
+        'sale_order_line_menu','sale_order_line_remaining_to_deliver'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
